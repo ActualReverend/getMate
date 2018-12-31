be3c8731a56ea3c2c87635b0f3897f847bd055cc
ActualReverend <bryan.loveless@gmail.com>
2018-12-30 19:16:47 -0700

    changed to function

--- getMateWORKS.py
+++ getMateWORKS.py
@@ -1,6 +1,11 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
-while True:
-    line = ser.readline()   # read a '\n' terminated line
-    print(line)
-ser.close()
+def getMate():
+    ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
+    while True:
+        line = ser.readline()   # read a '\n' terminated line
+        print(line)
+    ser.close()
+
+if __name__ == "__main__":
+    getMate()
+    pass
